e4bf471df5bc5eda14bc9b657b89e4a5e4e6150c
offyerrocker <offyerrocker88@gmail.com>
2021-07-19 20:32:48 -0700

    more framework progress
    Added sliders for scale (mostly placeholders atm)
    More basic HUD progress for visual setters
    Better asset loading for HUD required textures
    HUD creation/alignment progress (main player only atm)

--- lua/hudteammate.lua
+++ lua/hudteammate.lua
@@ -1,150 +1,162 @@
---[[
-
-> creation notes: 
-	player: 
-		- main panel has default alignment
-		- xp bar is aligned to bottom
-		- level/infamy text is aligned to center
-		- health/armor aligned left of level text, on top of xp bar
-		- hunger aligned right of level text, on top of xp bar
-		- item bar:
-			1. weapon1
-			2. weapon2
-			3. throwable/cooldown
-			4. deployable1
-			5. deployable2
-			6. cableties
-			7. bodybags
-			8. lootbag
-		- inventory keybind? or tabscreen:
-			- mission equipments
-			- pagers
-		
-		- boss hp bar:
-			- assault progress? currently aimed at enemy?
-		
-		- buff counter bar in inventory screen?
-			- tfw minecrafthud unironically is an infohud 
-		- additional information:
-			- current track?
-			- hostages/jokers?
-		- can't forget that good default minecraft crosshair (subtract blend mode)
-		
-		- nametag on bottom? 
-	team:
-		- name
-		- level/infamy
-		- health/armor
-		- throwable
-		- deployable(s)
-		- cableties
-		- lootbag
-	general:
-		- main menu overhaul?
-		- chat overhaul?
-
-
-
-
-> assets todo:
-	* non-outlined minecraft font
+Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,_i, teammates_panel, is_player, width)
+	if not self._main_player then return end
 	
-
-> how am i going to make the character in the inventory screen
-
---]]
-
-
-Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammates_panel, is_player, width)
+--get some useful minecrafthud "tweakdata" values
 	local mctd = MinecraftHUD._hud_data
-
+	local texture_data = MinecraftHUD._textures
 	local scale = 1
-	--todo scale from settings
 	if self._main_player then
-		scale = scale * 1
+		scale = scale * MinecraftHUD:GetPlayerScale()
 	else
-		scale = scale * 0.5
+		scale = scale * MinecraftHUD:GetTeammateScale()
 	end
-	
 	local HEALTH_TICKS = mctd.health_ticks
 	local HUNGER_TICKS = mctd.hunger_ticks
 	local ARMOR_TICKS = mctd.armor_ticks
 	local HOTBAR_SLOTS = mctd.hotbar_slots
 	
+	local get_icon = MinecraftHUD.get_atlas_icon
 	
-	
+--main mchud panel for any given criminal
 	local teammate_panel = teammates_panel:panel({
 		name = "mchud_" .. tostring(i)
 	})
+
+--set some useful alignment variables
+	local hotbar_bottom_margin = 64 --space between hotbar bottom and screen bottom edge
+	local xp_bottom_margin = 10 --space between xp bar bottom and hotbar
+	local vitals_bottom_margin = 4 --space between vitals bottom and xp bar
+	local center_y = teammate_panel:h() / 2
+	local center_x = teammate_panel:w() / 2
 	
-	local hud_center_x = teammate_panel:w() / 2
-	local hud_center_y = teammate_panel:h() / 2
-	
-	
-	
-	local vitals_panel = teammate_panel:panel({
-		name = "vitals_panel",
-		x = 0,
-		y = 0
+--subpanel creation
+	local hotbar_panel = teammate_panel:panel({
+		name = "hotbar_panel",
+		visible = self._main_player
 	})
+	hotbar_panel:set_bottom(-hotbar_bottom_margin)
+	
+	local hotbar_bg = hotbar_panel:bitmap({
+		name = "hotbar_bg",
+		texture = texture_data.hotbar.path
+	})
+	--todo populate hotbar
+	for i=1,HOTBAR_SLOTS,1 do
+		local item_w = 36 * scale
+		local item_h = 36 * scale
+		local item_offset_x = 0
+		local item = hotbar:panel({
+			name = "item_" .. tostring(i),
+			x = item_offset_x + (item_w * scale),
+			y = 0,
+			w = item_w,
+			h = item_h,
+			visible = false
+		})
+		local counter_top = item:text({
+			name = "counter_top",
+			font = font_data.minecraft,
+			font_size = 12,
+			text = "",
+			color = Color.white,
+			align = "right",
+			vertical = "top"
+		})
+		local counter_bottom = item:text({
+			name = "counter_bottom",
+			font = font_data.minecraft,
+			font_size = 12,
+			text = "",
+			color = Color.white,
+			align = "right",
+			vertical = "bottom"
+		})
+		local bitmap = item:bitmap({
+			name = "bitmap",
+			texture = ""
+		})
+		bitmap:set_center(item:center())
+		
+		local debug_rect = item:rect({
+			name = "debug",
+			color = Color(math.random(),math.random(),math.random()),
+			alpha = 0.1
+		})
+	end
+	
 	
 	local xp_panel = teammate_panel:panel({
 		name = "xp_panel",
-		x = 0,
-		y = 0
+		w = 700,
+		h = 400
 	})
+	xp_panel:set_bottom(hotbar_panel:y() - xp_bottom_margin)
 	
-
-	local xp_empty = xp_panel:bitmap({ --728 20
+	local xp_empty = xp_panel:bitmap({
 		name = "xp_empty",
-		texture = "textures/minecraft_xp_empty",
+		texture = texture_data.xp_empty.path,
 		layer = 2
 	})
-	xp_empty:set_size(xp_empty:w() / 2,xp_empty:h() / 2)
+	--todo resize based on scale
+--	xp_empty:set_size(xp_empty:w() / 2,xp_empty:h() / 2)
+	
+	xp_empty:set_x((xp_panel:w() - xp_empty:w()) / 2)
+	xp_empty:set_y(xp_panel:h() - (xp_empty:h() + xp_bottom_margin))
+	
+--[[
 	
 	xp_empty:set_x((xp_panel:w() - xp_empty:w()) / 2)
 	xp_empty:set_y(hotbar:y() - (xp_empty:h() + 8))
-	
+--]]
 	local xp_full = xp_panel:bitmap({
 		name = "xp_full",
-		texture = "textures/minecraft_xp_full",
-		layer = 3
+		texture = texture_data.xp_full.path,
+		x = xp_empty:x(),
+		y = xp_empty:y(),
+		layer = 4
 	})
-	xp_full:set_size(xp_full:w() / 2,xp_full:h() / 2)
+--	xp_full:set_size(xp_full:w() / 2,xp_full:h() / 2)
 	
-	xp_full:set_x((xp_panel:w() - xp_full:w()) / 2)
-	xp_full:set_y(hotbar:y() - (xp_full:h() + 8))
-	
+	local xp_potential = xp_panel:bitmap({
+		name = "xp_potential",
+		texture = texture_data.xp_full.path,
+		x = xp_empty:x(),
+		y = xp_empty:y(),
+		layer = 3,
+		color = MinecraftHUD._color_data.xp_bar_potential,
+		visible = false
+	})
+
 	local level = xp_panel:text({
 		name = "level",
-		text = tostring(managers.experience and managers.experience:current_level() or "7"),
-		color = Color("B7FD98"),
-		font = self._fonts.minecraft,
+		text = tostring(managers.experience and managers.experience:current_level() or "0"),
+		color = MinecraftHUD._color_data.xp_counter,
+		font = MinecraftHUD._fonts.minecraft,
 		font_size = 24,
-		layer = 3,
-		align = "center"
+		layer = 5,
+		align = "center",
+--		vertical = "bottom",
+		y = xp_empty:y()
 	})
 
 
-	local hotbar_panel = teammate_panel:panel({
-		name = "hotbar_panel",
-		y = -164,
-		visible = self._main_player
+	
+
+	--health/armor/stamina etc
+	local vitals_panel = teammate_panel:panel({
+		name = "vitals_panel",
+		w = 700,
+		h = 400,
+		x = 0,
+		y = 0
 	})
-	local hotbar_bitmap = hotbar_panel:bitmap({
-		name = "hotbar_bitmap",
-		texture = ""
-	})
-	
-	local center_x = teammate_panel:w() / 2
-	local health_y = hotbar:y() - (size * 2)
-	
-	local get_icon = mctd.get_icon
-	
+	vitals_panel:set_bottom(xp_panel:y() - vitals_bottom_margin)
+	local health_y = vitals_panel:h()
+
 	for i = 1,ARMOR_TICKS do 
 		vitals_panel:bitmap({
-			name = "armor_" .. i,
-			texture = "textures/minecraft_atlas",
+			name = "armor_tick_" .. i,
+			texture = texture_data.atlas.path,
 			texture_rect = get_icon("armor_full"),
 			w = DEFAULT_SIZE,
 			h = DEFAULT_SIZE,
@@ -155,8 +167,8 @@ Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammat
 	end
 	for i = 1,HEALTH_TICKS do
 		vitals_panel:bitmap({
-			name = "health_fill_" .. i,
-			texture = "textures/minecraft_atlas",
+			name = "health_tick_" .. i,
+			texture = texture_data.atlas.path,
 			texture_rect = get_icon("health_heart_full"),
 			w = DEFAULT_SIZE,
 			h = DEFAULT_SIZE,
@@ -164,6 +176,7 @@ Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammat
 			y = health_y,
 			layer = 3
 		})
+		--[[
 		vitals_panel:bitmap({
 			name = "health_bg_" .. i,
 			texture = "textures/minecraft_atlas",
@@ -174,12 +187,13 @@ Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammat
 			y = health_y,
 			layer = 2
 		})
+		--]]
 	end
 	
 	for i = 1,HUNGER_TICKS do 
 		vitals_panel:bitmap({
-			name = "hunger_fill_" .. i,
-			texture = "textures/minecraft_atlas",
+			name = "hunger_tick_" .. i,
+			texture = texture_data.atlas.path,
 			texture_rect = get_icon("hunger_heart_full"),
 			w = DEFAULT_SIZE,
 			h = DEFAULT_SIZE,
@@ -187,6 +201,7 @@ Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammat
 			y = health_y,
 			layer = 3
 		})
+		--[[
 		vitals_panel:bitmap({
 			name = "hunger_bg_" .. i,
 			texture = "textures/minecraft_atlas",
@@ -197,16 +212,12 @@ Hooks:PostHook(HUDTeammate,"init","mchud_teammate_init",function(self,i, teammat
 			y = health_y,
 			layer = 2
 		})
+		--]]
 	end
 	
-	
-
-	
-	
-	
-	if self._main_player then 
-		vitals_panel:set_y(-164)
-	end
 	
 	
 end)
+
+
+
--- lua/menumanager.lua
+++ lua/menumanager.lua
@@ -15,6 +15,63 @@
 	settings:
 		- infamy, level, or both
 
+
+> creation notes: 
+	player: 
+		- main panel has default alignment
+		- xp bar is aligned to bottom
+		- level/infamy text is aligned to center
+		- health/armor aligned left of level text, on top of xp bar
+		- hunger aligned right of level text, on top of xp bar
+		- item bar:
+			1. weapon1
+			2. weapon2
+			3. throwable count
+			4. deployable1
+			5. deployable2
+			6. cableties
+			7. bodybags
+			8. lootbag
+		- minecraft crit meter is throwable cooldown meter
+		- inventory keybind? or tabscreen:
+			- mission equipments
+			- pagers
+		
+		- boss hp bar:
+			- assault progress? currently aimed at enemy?
+		
+		- buff counter bar in inventory screen?
+			- tfw minecrafthud unironically is an infohud 
+		- additional information:
+			- current track?
+			- hostages/jokers?
+		- can't forget that good default minecraft crosshair (subtract blend mode)
+		
+		- nametag on bottom? (menu option)
+	team:
+		- nametag above head
+			-upside down easter egg?
+		- level/infamy
+		- health/armor
+		- throwable
+		- deployable(s)
+		- cableties
+		- lootbag
+	general:
+		- main menu overhaul?
+		- chat overhaul?
+
+
+
+
+> assets todo:
+	* non-outlined minecraft font
+	* hotbar selection square
+	* minecraft bow icon (default weapon)
+		- draw "animation" on hotbar when adsing?
+
+> how am i going to make the character in the inventory screen
+
 --]]
 
 MinecraftHUD = MinecraftHUD or {}
@@ -22,9 +79,14 @@ MinecraftHUD._path = MinecraftHUD._path or ModPath
 MinecraftHUD._save_path = MinecraftHUD._save_path or SavePath
 MinecraftHUD._data_path = MinecraftHUD._data_path or (MinecraftHUD._save_path .. "minecrafthud.json")
 MinecraftHUD._assets_path = MinecraftHUD._assets_path or (MinecraftHUD._path .. "assets/")
-MinecraftHUD._assets_path = MinecraftHUD._assets_path or (MinecraftHUD._path .. "assets/")
 MinecraftHUD._main_menu_path = MinecraftHUD._main_menu_path or (MinecraftHUD._path .. "menu/options.json")
 MinecraftHUD._default_localization_file = MinecraftHUD._default_localization_path or "english.json"
+
+
+MinecraftHUD._color_data = {
+	xp_counter = Color("B7FD98"),
+	xp_bar_potential = Color("888800")
+}
 
 MinecraftHUD._fonts = {
 	minecraft = "fonts/minecraftia_outline",
@@ -33,29 +95,56 @@ MinecraftHUD._fonts = {
 
 MinecraftHUD._textures = {
 	atlas = {
-		file_name = "minecraft_atlas",
-		local_path = "guis/textures/mchud/hud"
+		path = "guis/textures/mchud/hud/minecraft_atlas",
+		extension = "texture"
 	},
 	hotbar = {
-		file_name = "minecraft_hotbar"
+		path = "guis/textures/mchud/hud/minecraft_hotbar",
+		extension = "texture"
 	},
+	--[[
+	hotbar_selection = {
+		--none yet
+	},
+	bow_icon = {
+		--none yet
+	},
+	--]]
 	xp_empty = {
-		file_name = "xp_empty"
+		path = "guis/textures/mchud/hud/minecraft_xp_empty",
+		extension = "texture"
 	},
 	xp_full = {
-		file_name = "xp_full"
+		path = "guis/textures/mchud/hud/minecraft_xp_full",
+		extension = "texture",
+		size = {
+			728, 20
+		}
 	}
 }
+MinecraftHUD._weapon_icons_path = "guis/textures/mchud/vanilla_guns/"
 MinecraftHUD._weapon_icons = {
 	--auto-generated
 }
---for filename,extension in weapon_icons_folder do
--- if extension == "png" then 
---    load texture
--- end
---end
 
-MinecraftHUD._sounds = {
+function MinecraftHUD:LoadWeaponIcon(id)
+	local icon_data = id and self._weapon_icons[id] 
+	if icon_data then 
+		local path = icon_data.path
+		local asset_path = icon_data.asset_path
+		local texture_ids = Idstring("texture")
+		if DB:has(texture_ids, path) then 
+			--do nothing; icon is already loaded
+		else
+			self:log("Loaded weapon icon [" .. tostring(id) .. "]")
+			BLT.AssetManager:CreateEntry(Idstring(path),texture_ids,asset_path)
+		end
+	else
+		self:log("ERROR: No weapon icon found by id [" .. tostring(id) .. "]!")
+	end
+end
+
+MinecraftHUD._sounds = { --not done/used
 	player_hit = {
 		file = "oof",
 		type = "ogg"
@@ -87,27 +176,64 @@ MinecraftHUD._hud_data = {
 		hunger_transparent_full = {5,2},
 		hunger_transparent_half = {6,2}
 	},
-	atlas_name = "textures/minecraft_atlas"
-	get_icon = function(name)
-		local item = (type(name) == "table" and name) or MinecraftHUD._hud_data.atlas[name]
-		if item then 
-			local x,y,size = unpack(item)
-			size = size or MinecraftHUD._hud_data.size
-			return {size * x, size * y, size, size}
-		end
-		return {0,0,size,size}
+	atlas_name = "textures/minecraft_atlas", --not used
+	get_icon = function(...)
+		MinecraftHUD:log("get_icon() is deprecated! Please use MinecraftHUD.get_atlas_icon() instead!")		
+		return MinecraftHUD.get_atlas_icon(...)
 	end
 }
 
+function MinecraftHUD.get_atlas_icon(name)
+	local item = (type(name) == "table" and name) or MinecraftHUD._hud_data.atlas[name]
+	if item then 
+		local x,y,size = unpack(item)
+		size = size or MinecraftHUD._hud_data.size
+		return {size * x, size * y, size, size}
+	end
+	return {0,0,size,size}
+end
 
 MinecraftHUD.settings = MinecraftHUD.settings or {}
 MinecraftHUD.default_settings = MinecraftHUD.default_settings or {
 	enabled = true,
+	player_hud_scale = 1,
+	team_hud_scale = 0.5,
 	player_vitals_display_mode = 1, --1: faithful, 2: helpful
 	team_vitals_display_mode = 1
 }
 
-function MinecraftHUD:IsEnabled()
+MinecraftHUD._cache = {
+	--store session-specific data
+	teammate_panels = {
+		--indexed 1-4, contains data in addition to hud panel, eg:
+		--[[
+		{
+			armor_ticks = 5
+			health_ticks = 5,
+			panel = Panel
+		}
+		
+		--]]
+	}
+}
+
+function MinecraftHUD:log(a,...)
+	if Console then 
+		Console:Log("MinecraftHUD: " .. tostring(a),...)
+	else
+		log("MinecraftHUD: " .. tostring(a),...)
+	end
+end
+
+function MinecraftHUD:GetPlayerScale()
+	return self.settings.player_hud_scale
+end
+
+function MinecraftHUD:GetTeammateScale()
+	return self.settings.teammate_hud_scale
+end
+
+function MinecraftHUD:IsEnabled() --not used
 	return self.settings.enabled
 end
 
@@ -119,6 +245,219 @@ function MinecraftHUD:GetTeamVitalsDisplayMode()
 	return self.settings.team_vitals_display_mode
 end
 
+
+-------------------------------visual HUD setters
+function MinecraftHUD:SetExperienceProgress(percent)
+	do return end
+	local scale = self:GetPlayerScale()
+	local w,h = unpack(self._textures.xp_full.size)
+	xp_bar_full:set_w(percent * w * scale)
+	xp_bar_full:set_texture_rect(0,0,percent * w,h)
+	
+end
+
+--sets the amount of potential xp gained from 
+function MinecraftHUD:SetExperiencePotential(xp)
+	if not managers.experience then 
+		return
+	end
+	local current_amount = 0
+	local needed_amount_to_next_level = 0
+	local total_amount_to_next_level = 0
+	
+	local percent = math.min((current_amount + xp) / total_amount_to_next_level,1)
+	
+	do return end
+	local scale = self:GetPlayerScale()
+	local w,h = unpack(self._textures.xp_full.size)
+	xp_bar_potential:set_w(percent * w * scale)
+	xp_bar_potential:set_texture_rect(0,0,percent * w,h)
+end
+
+function MinecraftHUD:SetHealth(i,current,total)
+	local data = self._cache.teammate_panels[i]
+	if not data then 
+--		self:log("ERROR: No teammate panel found: " .. tostring(i))
+		return
+	end
+	local get_icon = self.get_atlas_icon
+	local HEALTH_TICKS = data.health_ticks or self._hud_data.health_ticks
+	
+	local current_ratio = current
+	if not current then 
+		return
+	elseif total then 
+		current_ratio = (current / total)
+	end
+	current_ratio = 1 - current_ratio
+	
+	local panel = data.panel
+	local vitals_panel = panel:child("vitals_panel")
+	
+	for i = HEALTH_TICKS,1,-1 do 
+		local tick = vitals_panel:child("health_tick_" .. i)
+		if alive(tick) then 
+			local tick_ratio = (i - 1) / HEALTH_TICKS 
+			if math.floor(tick_ratio * 10) >= math.floor(current_ratio * 10) then
+				if tick_ratio * 10 < current_ratio * 10 then 
+					tick:set_image(self._textures.atlas,unpack(get_icon("health_heart_half")))
+				else
+					tick:set_image(self._textures.atlas,unpack(get_icon("health_heart_full")))
+				end
+--				tick:show()
+			else
+				tick:set_image(self._textures.atlas,unpack(get_icon("health_empty_black")))
+--				tick:hide()
+			end
+		else
+			break
+		end
+	end
+end
+
+function MinecraftHUD:SetArmor(i,current,total)
+	local data = self._cache.teammate_panels[i]
+	if not data then 
+--		self:log("ERROR: No teammate panel found: " .. tostring(i))
+		return
+	end
+	local get_icon = self.get_atlas_icon
+	local ARMOR_TICKS = data.armor_ticks or self._hud_data.armor_ticks
+	
+	
+	if not (total and current and total > 0) then 
+		return
+	end
+	local current_ratio = 1 - (current / total)
+	
+	local panel = data.panel
+	local vitals_panel = panel:child("vitals_panel")
+	
+	for i = ARMOR_TICKS,1,-1 do 
+		local tick = vitals_panel:child("armor_tick_" .. i)
+		if alive(tick) then 
+			local tick_ratio = (i - 1) / ARMOR_TICKS 
+			if math.floor(tick_ratio * 10) >= math.floor(current_ratio * 10) then
+				if tick_ratio * 10 < current_ratio * 10 then 
+					tick:set_image(self._textures.atlas,unpack(get_icon("armor_half")))
+				else
+					tick:set_image(self._textures.atlas,unpack(get_icon("armor_full")))
+				end
+			else
+				tick:set_image(self._textures.atlas,unpack(get_icon("armor_empty")))
+			end
+		else
+			break
+		end
+	end
+end
+
+function MinecraftHUD:SetHunger(i,current,total)
+	local data = self._cache.teammate_panels[i]
+	if not data then 
+--		self:log("ERROR: No teammate panel found: " .. tostring(i))
+		return
+	end
+	local get_icon = self.get_atlas_icon
+	local HUNGER_TICKS = self._hud_data.hunger_ticks
+	
+	local current_ratio = current
+	if not current then 
+		return
+	elseif total then 
+		current_ratio = (current / total)
+	end
+	current_ratio = 1 - current_ratio
+	
+	local panel = data.panel
+	local vitals_panel = panel:child("vitals_panel")
+	
+	for i = 1,HUNGER_TICKS do 
+		local tick = panel:child("hunger_tick_" .. i)
+		local r = (i - 1) / HUNGER_TICKS 
+		if math.round(r * 10) >= current_ratio * 10 then
+			tick:show()
+			tick:set_image(data.atlas_name,unpack(get_icon("hunger_heart_full")))			
+		elseif math.round(r * 10) >= math.floor(current_ratio * 10) then
+--			tick:show()
+			tick:set_image(data.atlas_name,unpack(get_icon("hunger_heart_half")))
+		else
+			tick:set_image(data.atlas_name,unpack(get_icon("hunger_empty_black")))
+--			tick:hide()
+		end
+	end
+	
+end
+
+function MinecraftHUD:SetHealthTicks(i,num)
+	local data = self._cache.teammate_panels[i]
+	if data then 
+		data.health_ticks = num
+		if alive(data.panel) then 
+			local vitals_panel = data.panel:child("vitals_panel")
+			for i = 1,self._hud_data.health_ticks do 
+				local tick = vitals_panel:child("health_fill_" .. tostring(i))
+				if alive(tick) then 
+					--health is created right to left
+					tick:set_visible(i >= num)
+				else
+					break
+				end
+			end
+		end
+	end
+end
+
+function MinecraftHUD:SetHotbarIcon(i,icon,source,count1,count2)
+	local teammate_panel = self._cache.teammate_panels[HUDManager.PLAYER_PANEL]
+	local hotbar_panel = teammate_panel:child("hotbar_panel")
+	local item = hotbar_panel:child("item_" .. tostring(i))
+	if alive(item) then 
+		local counter_bottom = item:child("counter_bottom")
+		local counter_top = item:child("counter_top")
+		if count1 then 
+			counter_bottom:set_text(tostring(count1)
+		end
+		if count2 then 
+			counter_top:set_text(tostring(count2))
+		end
+		local bitmap = item:child("bitmap")
+		
+		if icon then
+			local texture,texture_rect
+			if source == "weapon" then 
+				if self._weapon_icons[icon] then 
+					texture = self._weapon_icons[icon].path
+				else
+					self:log("ERROR: No weapon icon found by name " .. tostring(icon))
+					return
+				end
+			elseif source == "atlas" then 
+				texture = self._textures.atlas.path
+				texture_rect = self.get_atlas_icon(icon)
+			end
+			
+			if texture then 
+				item:set_image(texture,texture_rect and unpack(texture_rect))
+			end
+		end
+	else
+		self:log("ERROR: Invalid hotbar index " .. tostring(i))
+	end
+end
+
+-------------------------------
+
+
+
+
+
+
+
+
+
+
+
 -------------------------------asset loading
 
 --Registers assets into the game's db so that they can be loaded later 
@@ -128,6 +467,7 @@ function MinecraftHUD:CheckResourcesAdded(skip_load)
 	
 	for font_id,font_path in pairs(self._fonts) do 
 		if DB:has(font_ids, font_path) then 
+			--font already loaded, do nothing
 --			self:log("Font " .. font_id .. " at path " .. font_path .. " is verified.")
 		else
 			--assume that if the .font is not loaded, then the .texture is not either (both are needed anyway)
@@ -141,15 +481,16 @@ function MinecraftHUD:CheckResourcesAdded(skip_load)
 	end
 	
 	for texture_id,texture_data in pairs(self._textures) do 
-		local asset_path = texture_data.asset_path
-		local local_path = texture_data.local_path
+		local path = texture_data.path
+		local extension = texture_data.extension or "texture"
 		local force_load = texture_data.force_load
-		if DB:has(texture_id,asset_path) and not force_load then 
-			--do nothing
+		if DB:has(texture_id,path) and not force_load then 
+--			self:log("Texture " .. texture_id .. " at path " .. path .. " is verified.")
+			--texture already loaded, do nothing
 		else
 			if not skip_load then 
-				local full_asset_path = self._assets_path .. asset_path
-				BLT.AssetManager:CreateEntry(Idstring(asset_path),texture_ids,full_asset_path .. ".texture")
+				local full_asset_path = self._assets_path .. path
+				BLT.AssetManager:CreateEntry(Idstring(path),texture_ids,full_asset_path .. "." .. extension)
 			end
 		end
 	
@@ -166,8 +507,7 @@ function MinecraftHUD:CheckFontResourcesReady(skip_load,done_loading_cb)
 	
 	local dyn_pkg = DynamicResourceManager.DYN_RESOURCES_PACKAGE
 
-	if done_loading_cb and done_loading_cb ~= false then 
-	
+	if done_loading_cb then 
 		done_loading_cb = function(done,resource_type_ids,resource_ids)
 			if done then 
 --				self:log("Completed manual asset loading for " .. tostring(resource_ids))
@@ -207,6 +547,12 @@ Hooks:Add("LocalizationManagerPostInit","mchud_load_localization",function(self)
 end)
 
 function MinecraftHUD:LoadSettings()
+	for k,v in pairs(self.default_settings) do 
+		if self.settings[k] == nil then 
+			self.settings[k] = v
+		end
+	end
+
 	local file = io.open(self._data_path, "r")
 	if (file) then
 		for k, v in pairs(json.decode(file:read("*all"))) do
@@ -232,18 +578,46 @@ Hooks:Add( "MenuManagerInitialize", "mchud_init_menumanager", function(menu_mana
 		MinecraftHUD:SaveSettings()
 	end
 	
-	MenuCallbackHandler.callback_mchud_menu_close = function(this)
+	MenuCallbackHandler.callback_mchud_set_player_hud_scale = function(self,item)
+		MinecraftHUD.settings.player_hud_scale = tonumber(item:value())
 		MinecraftHUD:SaveSettings()
+	end
+	MenuCallbackHandler.callback_mchud_set_team_hud_scale = function(self,item)
+		MinecraftHUD.settings.team_hud_scale = tonumber(item:value())
+		MinecraftHUD:SaveSettings()
+	end
+	
+	MenuCallbackHandler.callback_mchud_menu_close = function(self)
+--		MinecraftHUD:SaveSettings()
 	end
 	MenuHelper:LoadFromJsonFile(MinecraftHUD._main_menu_path, MinecraftHUD, MinecraftHUD.settings)
 end)
 
-for k,v in pairs(MinecraftHUD.default_settings) do 
-	if MinecraftHUD.settings[k] == nil then 
-		MinecraftHUD.settings[k] = v
-	end
-end
-
 MinecraftHUD:LoadSettings()
+
+--register weapon icons, to be loaded later
+local get_files = SystemFS and callback(SystemFS,SystemFS,"list") or file and callback(file,file,"GetFiles")
+if get_files then 
+	local texture_ids = Idstring("texture")
+	local assets_path = MinecraftHUD._assets_path
+	local icons_path = MinecraftHUD._weapon_icons_path
+	for _,filename in get_files(assets_path .. icons_path) do
+		local extension = "png"
+		if extension == "png" then 
+			MinecraftHUD:log("Registering custom weapon icon for weapon with id: [" .. filename .. "]")
+			
+			local path = icons_path .. filename
+			MinecraftHUD._weapon_icons[filename] = {
+				id = filename, --not used but just in case
+				path = path,
+				asset_path = assets_path .. path .. "." .. extension
+			}
+		end
+	end
+	
+	--unload all here? not sure if textures are unloaded from memory between states
+else
+	MinecraftHUD:log("ERROR: could not load/register weapon icon pngs!")
+end
 
 MinecraftHUD:CheckResourcesAdded()
